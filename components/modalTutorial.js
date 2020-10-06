9c4d2e1e82a9403e5bc8db774761766469196136
jesuscovam <jesuscovam@icloud.com>
2020-10-06 07:05:58 -0500

    p-test

--- components/modalTutorial.js
+++ components/modalTutorial.js
@@ -10,7 +10,7 @@ const ModalTutorial = () => {
   const [modal, setModal] = useState(false)
   const toggleModal = () => setModal(val => !val)
   return (
-    <div>
+    <div className='p-10'>
       <button onClick={toggleModal} className='px-4 py-2 rounded shadow bg-pink-500 text-white focus:outline-none'>toggle modal</button>
 
       <ModalLogin modal={modal} toggleModal={toggleModal} />
